31a9d4c28d369c8fde493398c215bc96b173ebae
jprinci31-dot <jprinci31@gmail.com>
2026-07-01 13:41:12 +0530

    Create script.js

---
+++ css/css/script.js
@@ -0,0 +1,17 @@
+function playMusic() {
+  document.getElementById("music").play();
+}
+
+const weddingDate = new Date("February 2, 2027 19:00:00").getTime();
+
+setInterval(function () {
+  const now = new Date().getTime();
+  const distance = weddingDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+  document.getElementById("countdown").innerHTML =
+    days + " Days " + hours + " Hours " + minutes + " Minutes";
+}, 1000);
